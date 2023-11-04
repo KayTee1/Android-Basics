47fc7608b4e2775edff1afffe57ddf2edff4d3c0
KayTee1 <kha.truong@tuni.fi>
2023-11-04 15:15:01 +0200

    did some more of deviceapidemo

--- DeviceApiDemo/app/src/main/java/com/example/deviceapidemo/MainActivity.java
+++ DeviceApiDemo/app/src/main/java/com/example/deviceapidemo/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -26,6 +29,27 @@ public class MainActivity extends AppCompatActivity {
 
         for ( Sensor s : sensors){
             Toast.makeText(this, s.getName(), Toast.LENGTH_SHORT).show();
+        }
+
+        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (accelerometer != null){
+            sensorManager.registerListener(new SensorEventListener() {
+                @Override
+                public void onSensorChanged(SensorEvent event) {
+                    float x, y, z;
+                    x = event.values[0];
+                    y = event.values[1];
+                    z = event.values[2];
+                    TextView sensorTextView = findViewById(R.id.sensorTextView);
+                    sensorTextView.setText(String.format("X: %.2f Y: %.2f Z: %.2f", x, y, z));
+
+                }
+                //8:00 rec
+                @Override
+                public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+                }
+            }, accelerometer, SensorManager.SENSOR_DELAY_UI);
         }
     }
 
